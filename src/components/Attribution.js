c33efe1d67f36d02c5a454e5806ca90772fc2920
Dhruvik0257 <dhruvikdudhat007@gmail.com>
2024-07-09 15:59:23 +0530

    backgroud color diff

---
+++ src/components/Attribution.js
@@ -0,0 +1,7 @@
+export default function Attribution() {
+   return (
+      <footer className="attribution">
+         <p>developed by <a href="" target="_blank" rel="noreferrer">Dhruvik Patel</a></p>
+      </footer>
+   );
+}
